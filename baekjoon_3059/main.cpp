bef55bc6652d2ee160fd3b06071688261d9bbd09
WaniFi <skelpd24@gmail.com>
2018-08-19 18:10:36 +0900

    3059 문제 반복문 1개 줄임

--- baekjoon_3059/main.cpp
+++ baekjoon_3059/main.cpp
@@ -2,19 +2,19 @@
 #include <string.h>
 using namespace std;
 
-int t, al[26], r;
+int t, al[26], r, i, j;
 char i1[1001];
 int main() {
 	cin >> t;
-	for (int i = 0; i < t; i++) {
+	for (i = 0; i < t; i++) {
 		cin >> i1;
-		for (int j = 0; j < strlen(i1); j++) al[i1[j] - 'A']++;
-		for (int j = 0; j < 26; j++) {
-			if (al[j] == 0) r += j + 'A';
-			al[j] = 0;
+		r = 2015;
+		memset(al, 0, sizeof(al));
+		for (j = 0; j < strlen(i1); j++){
+			if (al[i1[j] - 'A'] == 0) r -= i1[j];
+			al[i1[j] - 'A']++;
 		}
 		cout << r << endl;
-		r = 0;
 	}
 	return 0;
 }
